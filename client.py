8668ccdcfb8866c02db6b4f301c25915ac2158c4
Flask <mkowalski234@gmail.com>
2022-06-05 15:07:56 +0200

    done i hate myself

--- client.py
+++ client.py
@@ -1,22 +1,32 @@
+import threading
 import socket
-
-HEADER = 64
-PORT = 5052
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.111"
-ADDR = (SERVER, PORT)
-
+alias = input('Choose an alias >>> ')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+client.connect(('192.168.1.111', 5900))
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_lenght = str(msg_length).encode(FORMAT)
-    send_lenght += b' '*(HEADER-len(send_lenght))
-    client.send(send_lenght)
-    client.send(message)
-    print(client.recv(2048).decode(FORMAT))
-while send(input()) != "quit":
-    send(input())
+
+def client_receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "alias?":
+                client.send(alias.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('Error!')
+            client.close()
+            break
+
+
+def client_send():
+    while True:
+        message = f'{alias}: {input("")}'
+        client.send(message.encode('utf-8'))
+
+
+receive_thread = threading.Thread(target=client_receive)
+receive_thread.start()
+
+send_thread = threading.Thread(target=client_send)
+send_thread.start()
--- server.py
+++ server.py
@@ -1,43 +1,46 @@
-import socket
 import threading
-
-HEADER = 64
-PORT = 5052
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-
+import socket
+host = '192.168.1.111'
+port = 5900
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+server.bind((host, port))
+server.listen()
+clients = []
+aliases = []
 
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected" )
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
-    connected = True
-    while connected:
-        msg_lenght = conn.recv(HEADER).decode(FORMAT)
-        if msg_lenght:
-            msg_lenght = int(msg_lenght)
-            msg = conn.recv(msg_lenght).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-            if msg_lenght:
-                conn.send("message recieved".encode(FORMAT))
-            print(f"[{addr}] {msg}")
-
-    conn.close()
-
-def start():
-    server.listen()
-    print(f"[Listening] server is listening on {SERVER}")
+def handle_client(client):
     while True:
-
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args= (conn, addr))
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            alias = aliases[index]
+            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
+            aliases.remove(alias)
+            break
+def receive():
+    while True:
+        print('Server is running and listening ...')
+        client, address = server.accept()
+        print(f'{str(address)} just joined')
+        client.send('who are you ??'.encode('utf-8'))
+        alias = client.recv(1024)
+        aliases.append(alias)
+        clients.append(client)
+        print(f'Everyone its {alias}'.encode('utf-8'))
+        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+        client.send('you are now connected!'.encode('utf-8'))
+        thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-print("Server is starting...")
-start()
+
+if __name__ == "__main__":
+    receive()
